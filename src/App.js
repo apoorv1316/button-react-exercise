5ca1dac9539d3c8f9a4dfc5ac0a9abefbae36ddc
apoorv1316 <tiwari.apoorv1316@gmail.com>
2020-06-10 19:57:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from './Component/Button'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+      <h4>Button Types</h4>
+      <Button text="solid" type="solid"  />
+      <Button text="outlined" type="outlined" />
+      <Button text="flat" type="flat" />
+      <h4>Button Size</h4>
+     <Button text="large" type="large" />
+     <Button text="medium" type="medium" />
+     <Button text="small" type="small" />
+     <Button text="flat small" type="flat" size="small" />
+     <Button text="outlined small" type="outlined" size="small" />
+     <h4>Button With Icon</h4>
+     <Button size="large" type="flat" icon={true} text="SAMPLE TEXT" />
+    <Button size="large" icon={true} text="SAMPLE TEXT" />
+    <Button icon="true" text="SAMPLE TEXT" />
+    <Button type="outlined" icon={true} text="SAMPLE TEXT"/>
+      </div>
+     
     </div>
   );
 }
